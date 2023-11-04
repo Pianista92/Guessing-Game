742ee4ca21a8acbdbac8b40d0cdc98a334290073
username <useremail>
2023-11-04 12:42:29 -0700

    Add project files.

---
+++ GuessingGame/GuessingGame.py
@@ -0,0 +1,52 @@
+guess = input("Guess the number!")
+
+print("Enter the limit: 10")
+
+guess = input("I'm thinking of a number from 1 to 10")  
+
+userinput = "5"
+
+guess = input("Your guess: 5")
+
+guess = input("Too low")
+
+userinput = "3"
+
+guess = input("Your guess: 3")
+
+guess = input("Too low")
+
+userinput = "10"
+
+guess = input("Your guess:10")
+
+guess = input("You guessed it in 4 tries")
+
+guess = input("Would you like to play again? (y/n)")
+
+userinput = "y"
+
+print("Enter the limit: 20")
+
+print("I'm thinking of a number from 1 to 20")
+
+userinput = "20"
+
+guess = input("Too high")
+
+userinput = "10"
+
+guess = input("Too low")
+
+userinput = "15"
+
+guess = input("You got it in 3 tries")
+
+guess = input("Would you like to play again? (y/n)")
+
+userinput = "n"
+
+print("Bye!")
+
+
+
